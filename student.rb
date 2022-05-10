54b73e68e845c12edb290c249ad085a97efa024d
cynthiainga <candyinga67@gmail.com>
2022-05-10 11:52:16 +0200

    Add tests.yml file

--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
 require_relative 'person'
 
 class Student < Person
-    attr_reader :classroom
-    
-    def initialize(name = "Unknown", age, classroom, parent_permission: true)
+    attr_accessor :classroom
+
+    def initialize(age, name = "Unknown", classroom, parent_permission: true)
         super(name, age, parent_permission: parent_permission)
         @classroom = Classroom
     end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,9 @@
-require_relative 'person.rb'
+require_relative 'person'
 
 class Teacher < Person
-    def initialize(name = "Unknown", age, specialization, parent_permission: true)
+    attr_accessor :specialization
+    
+    def initialize(age, name = "Unknown", specialization, parent_permission: true)
         super(name, age, parent_permission: parent_permission)
         @specialization = specialization
     end
