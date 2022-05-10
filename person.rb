62306e9f82d6cf94c1cd33285ead825dcffc6afa
cynthiainga <candyinga67@gmail.com>
2022-05-10 11:55:52 +0200

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(age, name = "Unknown", parent_permission: true)
-        @id = Random.ran(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.ran(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 
-    private
+  private
 
-    def is_of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,12 @@
 require_relative 'person'
 
 class Student < Person
-    attr_accessor :classroom
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def initialize(age, name = "Unknown", classroom, parent_permission: true)
-        super(name, age, parent_permission: parent_permission)
-        @classroom = Classroom
-    end
-
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
