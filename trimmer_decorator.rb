fd8f0c2675fa9137e6a6d7d37caf4ba1a9e3ace2
cynthiainga <candyinga67@gmail.com>
2022-05-11 10:43:31 +0200

    Fix linters errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,9 +3,9 @@ require_relative 'base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
     if @nameable.correct_name.length < 10
-        @nameable.correct_name
+      @nameable.correct_name
     else
-        @nameable.correct_name[0, 10]
+      @nameable.correct_name[0, 10]
     end
   end
 end
