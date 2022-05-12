12d38694da3fd6191d62f18eac3a8115aec5358e
cynthiainga <candyinga67@gmail.com>
2022-05-12 06:15:14 +0200

    Creat the app.rb file

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.ran(1..1000)
+    @id = Random.ran(1..1000).to_i
     @name = name
     @age = age
     @parent_permission = parent_permission
