ef9d5e93d82bf585670103950aae5ab95c5bfb08
cynthiainga <candyinga67@gmail.com>
2022-05-13 17:02:10 +0200

    Add bug

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1000).to_i
+    @id = Random.rant(1..1000).to_i
     @name = name
     @age = age
     @parent_permission = parent_permission
